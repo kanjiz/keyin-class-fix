2d6d5c484e033be5fcb682f6e72083c20190725c
kanjiz <5319055+kanjiz@users.noreply.github.com>
2023-06-18 23:58:10 +0900

    style: Change KeyIn class indentation from 4 to 2 spaces
    Change the indentation size of the KeyIn class from 4 to 2 spaces to improve its readability and consistency with the project's coding style.

--- src/main/java/KeyIn.java
+++ src/main/java/KeyIn.java
@@ -1,22 +1,21 @@
 import java.io.*;
 
 public class KeyIn {
-    String buf = null;
-    BufferedReader br = 
-            new BufferedReader(new InputStreamReader(System.in));
+  String buf = null;
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public String readString() {
-        try {
-            buf = br.readLine();
-        } catch(IOException e) { 
-            System.out.println(e);
-            System.exit(1);
-        }
-        return buf;
+  public String readString() {
+    try {
+      buf = br.readLine();
+    } catch (IOException e) {
+      System.out.println(e);
+      System.exit(1);
     }
+    return buf;
+  }
 
-    public String readString(String msg) {
-        System.out.print(msg + ">");
-        return readString();
-    }
+  public String readString(String msg) {
+    System.out.print(msg + ">");
+    return readString();
+  }
 }
