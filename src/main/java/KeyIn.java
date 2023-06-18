32ba153d0ae53ad2334c299d8d0df7bf19ec4d5c
kanjiz <5319055+kanjiz@users.noreply.github.com>
2023-06-19 01:16:00 +0900

    Add Javadoc-style class and method comments to KeyIn class
    This commit adds Javadoc-style class and method comments to the KeyIn class in order to improve code readability and documentation. The class comment describes the purpose of the class and the methods, while the method comments describe the purpose of each method, its parameters, and its return value. This will make it easier for other developers to understand and use the KeyIn class in their own projects.

--- src/main/java/KeyIn.java
+++ src/main/java/KeyIn.java
@@ -2,10 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 標準入力から文字列を読み込むクラスです。
+ * 
+ * <p>
+ * このクラスは、標準入力から文字列を読み込むためのメソッドを提供します。
+ * </p>
+ */
 public class KeyIn {
   String buf = null;
   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+  /**
+   * 標準入力から文字列を読み込みます。
+   * 
+   * @return 標準入力から読み込んだ文字列
+   */
   public String readString() {
     try {
       buf = br.readLine();
@@ -16,6 +28,12 @@ public class KeyIn {
     return buf;
   }
 
+  /**
+   * プロンプトメッセージを表示して、標準入力から文字列を読み込みます。
+   * 
+   * @param msg プロンプトメッセージ
+   * @return 標準入力から読み込んだ文字列
+   */
   public String readString(String msg) {
     System.out.print(msg + ">");
     return readString();
